8cf43be675ae44d596da09c60f249aecb11bd951
paulo-vinicius23 <paulo.prado@aluno.uece.br>
2021-08-13 20:15:07 -0300

    delet add

--- src/main/java/com/APITeste/API/Controller/ControllerCategoria.java
+++ src/main/java/com/APITeste/API/Controller/ControllerCategoria.java
@@ -1,8 +1,10 @@
 package com.APITeste.API.Controller;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,21 +21,28 @@ public class ControllerCategoria {
 		@Autowired
 		CategoriaService categoria;
 		
-		@PostMapping(value = "/categoria", produces = MediaType.APPLICATION_JSON_VALUE)
+		@RequestMapping("/categoria/{id}")
 		@ResponseBody
 		public Optional<Categoria> getCategoria(@RequestParam Integer id){
 			return categoria.findById(id);
 		}
 		
-		@PostMapping(value = "/todas-categorias", produces = MediaType.APPLICATION_JSON_VALUE)
+		@GetMapping(value = "/todas-categorias", produces = MediaType.APPLICATION_JSON_VALUE)
 		@ResponseBody
-		public Iterable<Categoria> getAllCategoria(){
+		public List<Categoria> getAllCategoria(){
 			return categoria.findAll();
 		}
 		
-		@PostMapping(value = "/salvar-categoria", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+		@PostMapping(value = "/salvar-categoria/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 		@ResponseBody
 		public Categoria saveCategoria(@RequestBody Categoria cat){
 			return categoria.saveCategoria(cat);
+		}
+		
+		@GetMapping(value = "/delete-categoria/{id}")
+		@ResponseBody
+		public String deleteCategoriaById(@RequestParam Integer id){
+			categoria.deleteCategoria(id);
+			return "Categoria de id " + id + " deletada.";
 		}
 }
--- src/main/java/com/APITeste/API/Controller/ControllerFilme.java
+++ src/main/java/com/APITeste/API/Controller/ControllerFilme.java
@@ -1,8 +1,10 @@
 package com.APITeste.API.Controller;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,21 +20,28 @@ public class ControllerFilme {
 	@Autowired
 	FilmeService filme;
 	
-	@PostMapping(value = "/filme", produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping("/filme/{id}")
 	@ResponseBody
 	public Optional<Filme> getFilme(@RequestParam Integer id){
 		return filme.findById(id);
 	}
 	
-	@PostMapping(value = "/todos-filmes", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/todos-filmes", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public Iterable<Filme> getAllFilme(){
+	public List<Filme> getAllFilme(){
 		return filme.findAll();
 	}
 	
-	@PostMapping(value = "/salvar-filme", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/salvar-filme/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public Filme saveFilme(@RequestBody Filme fil){
 		return filme.saveFilme(fil);
+	}
+	
+	@GetMapping(value = "/delete-filme/{id}")
+	@ResponseBody
+	public String deleteFilmeById(@RequestParam Integer id) {
+		filme.deleteFilme(id);
+		return "Categoria de id " + id + " deletada.";
 	}
 }
